8fbd60690d5d8722c2e8e7f796581d68d2a36070
Wojciech Falkowski <wojciechxfalkowski@gmail.com>
2021-01-19 14:07:55 +0100

    add theme

---
+++ src/theme/theme.js
@@ -0,0 +1,22 @@
+const theme = {
+  spacing: {
+    xs: 4,
+    sm: 8,
+    mx: 16,
+    xl: 32,
+  },
+  breakpoints: {
+    bigDesktop: "1440",
+    desktop: "1024",
+    tablet: "768",
+    landscapePhone: "640",
+    phone: "360",
+  },
+  colors: {
+    white: "#fff",
+    orange: "#f8833f",
+    black: "#4e4e4e",
+    gray: "#e5e5e5",
+  },
+};
+export default theme;
